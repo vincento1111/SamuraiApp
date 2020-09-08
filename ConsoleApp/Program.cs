df38ac13826d3fec6fe91a4b0e809d84511b1817
Tec <Tec@SKAB6-PC-06>
2020-09-08 14:09:39 +0200

    Tirsdag opgave 2 næstne færdig

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static SamuraiContext context = new SamuraiContext();
+
+        static void Main(string[] args)
+        {
+            context.Database.EnsureCreated();
+            GetSamurais("Before Add:");
+            AddSamurai();
+            GetSamurais("After add:");
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Sampson" };
+            context.Samurais.Add(samurai);
+            context.SaveChanges();        
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
+        }
+    }
+}
