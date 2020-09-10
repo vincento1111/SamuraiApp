ca301681285995d61e9766f82fae5ee7c502fadb
vincent schuurhof <vincento2009@hotmail.com>
2020-09-10 22:06:19 +0200

    Opgave4

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -7,18 +7,23 @@ using System.Linq;
 
 namespace SamuraiApp.Data
 {
-    public class SamuraiContext: DbContext
+    public class SamuraiContext : DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
                 "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData2");
         }
-
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+        }
     }
 }
+    
--- SamuraiApp.Domain/Samurai.cs
+++ SamuraiApp.Domain/Samurai.cs
@@ -14,5 +14,7 @@ namespace SamuraiApp.Domain
         public string Name { get; set; }
         public List<Quote> Quotes { get; set; }
         public Clan Clan { get; set; }
+        public List<SamuraiBattle> SamuraiBattles { get; set; }
+        public Horse Horse { get; set; }
     }
 }
